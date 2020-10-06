03df7ce96dc78894d6e19f3bc9c93c1c3caba253
Gaya <caklovicka@gmail.com>
2020-10-06 17:02:03 +0200

    advection 72 test with new alphas (check)

--- main_advection_72.py
+++ main_advection_72.py
@@ -7,15 +7,15 @@ import seaborn as sns
 prob = Advection4()
 N = 1000
 prob.spatial_points = [N, N]
-prob.tol = 1e-11
-prob.proc_col = 72
+prob.tol = 1e-12
+prob.proc_col = 64
 prob.proc_row = 1
 prob.time_intervals = 1
 prob.rolling = 1
 prob.time_points = 3
 prob.optimal_alphas = True
 prob.T_start = 0
-prob.T_end = 0.009554572093283932
+prob.T_end = 0.001
 prob.solver = 'custom'
 prob.maxiter = 5
 prob.smaxiter = 50
@@ -23,6 +23,7 @@ prob.stol = 1e-15
 prob.m0 = 10 * (prob.T_end - prob.T_start)
 
 prob.setup()
+print(prob.dt/prob.dx[0])
 prob.solve()
 prob.summary(details=True)
 
