5cde03d345d3a31954cb29ae01b1d50fed6a12ea
Gaya <caklovicka@gmail.com>
2020-10-09 13:09:23 +0200

    setup test

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 prob = Heat4()
-N = 1100
+N = 1000
 prob.spatial_points = [N, N]
 prob.tol = 1e-12
 prob.proc_col = 3
@@ -39,7 +39,7 @@ prob.stol = 1e-14
 prob.m0 = 10 * (prob.T_end - prob.T_start)
 
 prob.setup()
-prob.solve()
+# prob.solve()
 # prob.summary(details=True)
 
 #
