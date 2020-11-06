c2a953af22e11f1d9409215e3519ffa28fbd2d9a
Gaya <caklovicka@gmail.com>
2020-11-06 16:26:55 +0100

    test

--- main.py
+++ main.py
@@ -25,22 +25,22 @@ from problem_examples_parallel.schrodinger_2d_central4 import Schrodinger as Sch
 from problem_examples_parallel.schrodinger_2d_0_central4 import Schrodinger as Schrodinger04
 from problem_examples_parallel.schrodinger_2d_0_central6 import Schrodinger as Schrodinger06
 
-prob = Schrodinger06()
-N = 1300
+prob = Advection2()
+N = 350
 prob.spatial_points = [N, N]
 prob.tol = 1e-5
-prob.proc_col = 3
-prob.time_intervals = 1
-prob.rolling = 4
+prob.proc_col = 1
+prob.time_intervals = 8
+prob.rolling = 1
 prob.proc_row = prob.time_intervals
-prob.time_points = 3
+prob.time_points = 2
 prob.optimal_alphas = True
 prob.T_start = 0
-prob.T_end = 5e-3
+prob.T_end = 1e-2
 prob.solver = 'custom'
 prob.maxiter = 5
 prob.smaxiter = 50
-prob.stol = 0.5e-13
+prob.stol = 1e-8
 prob.m0 = 1 * (prob.T_end - prob.T_start)
 
 prob.setup()
