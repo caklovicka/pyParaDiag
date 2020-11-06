fc7b314ee649bf5d6bc3c0774af31d2f2f710aa7
Gaya <caklovicka@gmail.com>
2020-11-06 17:45:42 +0100

    test

--- mainn.py
+++ mainn.py
@@ -25,22 +25,22 @@ from problem_examples_parallel.schrodinger_2d_central4 import Schrodinger as Sch
 from problem_examples_parallel.schrodinger_2d_0_central4 import Schrodinger as Schrodinger04
 from problem_examples_parallel.schrodinger_2d_0_central6 import Schrodinger as Schrodinger06
 
-prob = Schrodinger04()
-N = 1200
+prob = Schrodinger06()
+N = 1300
 prob.spatial_points = [N, N]
-prob.tol = 1e-9
+prob.tol = 1e-12
 prob.proc_col = 1
-prob.time_intervals = 16
-prob.rolling = 1
+prob.time_intervals = 1
+prob.rolling = 4
 prob.proc_row = prob.time_intervals
-prob.time_points = 2
+prob.time_points = 3
 prob.optimal_alphas = True
 prob.T_start = 0
 prob.T_end = 5e-3
 prob.solver = 'custom'
 prob.maxiter = 5
 prob.smaxiter = 50
-prob.stol = 1e-11
+prob.stol = 5e-14
 prob.m0 = 1 * (prob.T_end - prob.T_start)
 
 prob.setup()
