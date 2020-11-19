5d32b10ac1fb382bd43e69f16bedc1cd5bdc73fc
Gaya <caklovicka@gmail.com>
2020-11-19 12:43:31 +0100

    heat3 jube test

--- test_runs_and_plots/problem_scaling/heat3/run.py
+++ test_runs_and_plots/problem_scaling/heat3/run.py
@@ -22,7 +22,7 @@ prob.T_end = np.pi + 0.5
 prob.solver = 'custom'
 prob.maxiter = 10
 prob.smaxiter = 50
-prob.stol = 1e-15
+prob.stol = 1e-13
 prob.m0 = (prob.T_end - prob.T_start)/prob.rolling
 
 prob.setup()
