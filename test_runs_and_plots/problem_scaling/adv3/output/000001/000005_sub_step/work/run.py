70f6b768f871dfe2c9c76d9626c63d8bc517f71e
Gayatri ?aklovi? <aklovi1@jwlogin08.juwels>
2021-01-04 12:45:33 +0100

    juwels adv123

---
+++ test_runs_and_plots/problem_scaling/adv3/output/000001/000005_sub_step/work/run.py
@@ -0,0 +1,44 @@
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
+import sys
+sys.path.append('../../../../../../..')    # for jube
+sys.path.append('../../../../../../../..')    # for pySDC
+import numpy as np
+from problem_examples_parallel.advection_2d_pbc_upwind5 import Advection as Adv5
+
+prob = Adv5()
+N = 700
+prob.spatial_points = [N, N]
+prob.tol = 1e-12
+prob.proc_col = 1
+prob.time_points = 3
+prob.optimal_alphas = True
+prob.T_start = 0
+prob.solver = 'custom'
+prob.maxiter = 10
+prob.smaxiter = 50
+
+prob.stol = 1e-13
+if prob.rolling == 1:
+    prob.stol /= 100
+
+prob.m0 = (prob.T_end - prob.T_start)/prob.rolling
+
+prob.setup()
+prob.solve()
+prob.summary(details=True)
+
+if prob.rank == prob.size - 1:
+    exact = prob.u_exact(prob.T_end, prob.x).flatten()[prob.row_beg:prob.row_end]
+    approx = prob.u_last_loc.flatten()
+    d = exact - approx
+    d = d.flatten()
+    err_abs = np.linalg.norm(d, np.inf)
+    print('abs err = {}'.format(err_abs))
+
+
