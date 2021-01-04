fea3034fcb49c6caa0fb9a81f441095f63b9e342
Gaya <caklovicka@gmail.com>
2021-01-04 15:21:28 +0100

    schro test

--- test_runs_and_plots/problem_scaling/schro1/run.py
+++ test_runs_and_plots/problem_scaling/schro1/run.py
@@ -22,7 +22,10 @@ prob.T_start = 0
 prob.solver = 'custom'
 prob.maxiter = 10
 prob.smaxiter = 50
+
 prob.stol = 1e-6
+if prob.rolling == 1:
+    prob.stol /= 100
 
 prob.m0 = 10 * (prob.T_end - prob.T_start)/prob.rolling
 
