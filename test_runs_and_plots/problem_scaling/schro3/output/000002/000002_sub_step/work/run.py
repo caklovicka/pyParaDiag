fd054f287b92c507f794ccc764f0d3a335cd46e2
Gayatri ?aklovi? <aklovi1@jwlogin08.juwels>
2021-01-04 16:17:35 +0100

    juwels schro tests

---
+++ test_runs_and_plots/problem_scaling/schro3/output/000002/000002_sub_step/work/run.py
@@ -0,0 +1,43 @@
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
+import sys
+sys.path.append('../../../../../../..')    # for jube
+sys.path.append('../../../../../../../..')    # for pySDC
+import numpy as np
+from problem_examples_parallel.schrodinger_2d_0_central6 import Schrodinger as Schro6
+
+prob = Schro6()
+N = 1200
+prob.spatial_points = [N, N]
+prob.tol = 1e-12
+prob.proc_col = 1
+prob.time_points = 3
+prob.optimal_alphas = True
+prob.T_start = 0
+prob.solver = 'custom'
+prob.maxiter = 10
+prob.smaxiter = 50
+prob.stol = 1e-14
+if prob.rolling == 1:
+    prob.stol = 1e-16
+
+prob.m0 = 10 * (prob.T_end - prob.T_start)/prob.rolling
+
+prob.setup()
+prob.solve()
+prob.summary(details=True)
+
+if prob.rank == prob.size - 1:
+    exact = prob.u_exact(prob.T_end, prob.x).flatten()[prob.row_beg:prob.row_end]
+    approx = prob.u_last_loc.flatten()
+    d = exact - approx
+    d = d.flatten()
+    err_abs = np.linalg.norm(d, np.inf)
+    print('abs err = {}'.format(err_abs))
+
+
