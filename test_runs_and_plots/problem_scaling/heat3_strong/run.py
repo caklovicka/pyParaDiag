86e810cef2038168a02d29aeb0de9d064bd7ca25
Gaya <caklovicka@gmail.com>
2021-01-18 17:45:29 +0100

    heat3

--- test_runs_and_plots/problem_scaling/heat3_strong/run.py
+++ test_runs_and_plots/problem_scaling/heat3_strong/run.py
@@ -9,24 +9,22 @@ import sys
 sys.path.append('../../../../../../..')    # for jube
 sys.path.append('../../../../../../../..')    # for pySDC
 import numpy as np
-from problem_examples_parallel.schrodinger_2d_0_central6 import Schrodinger as Schro6
+from problem_examples_parallel.heat_2d_pbc_central6 import Heat as Heat6
 
-prob = Schro6()
-N = 1200
+prob = Heat6()
+N = 350
 prob.spatial_points = [N, N]
 prob.tol = 1e-12 / prob.rolling
 prob.proc_col = 1
 prob.time_points = 3
 prob.optimal_alphas = True
-prob.T_start = 0
+prob.T_start = np.pi
 prob.solver = 'custom'
 prob.maxiter = 10
 prob.smaxiter = 50
-prob.stol = 1e-14
-if prob.rolling == 1:
-    prob.stol = 1e-16
-
-prob.m0 = 10 * (prob.T_end - prob.T_start)/prob.rolling
+prob.stol = 1e-13
+prob.T_end += np.pi
+prob.m0 = (prob.T_end - prob.T_start)/prob.rolling
 
 prob.setup()
 prob.solve()
