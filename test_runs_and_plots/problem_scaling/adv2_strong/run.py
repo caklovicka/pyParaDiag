b840c23eee30be7d3fd337ed869d655e84b93c8a
Gaya <caklovicka@gmail.com>
2021-01-18 15:45:33 +0100

    adv2 strrong

--- test_runs_and_plots/problem_scaling/adv2_strong/run.py
+++ test_runs_and_plots/problem_scaling/adv2_strong/run.py
@@ -9,23 +9,23 @@ import sys
 sys.path.append('../../../../../../..')    # for jube
 sys.path.append('../../../../../../../..')    # for pySDC
 import numpy as np
-from problem_examples_parallel.advection_2d_pbc_upwind1 import Advection as Adv1
+from problem_examples_parallel.advection_2d_pbc_upwind3 import Advection as Adv3
 
-prob = Adv1()
+prob = Adv3()
 N = 800
 prob.spatial_points = [N, N]
-prob.tol = 1e-5 / prob.rolling
-prob.proc_col = 1
-prob.time_points = 1
+prob.tol = 1e-9 / prob.rolling
+prob.proc_col = 2
+prob.time_points = 2
 prob.optimal_alphas = True
 prob.T_start = 0
 prob.solver = 'custom'
 prob.maxiter = 10
 prob.smaxiter = 50
 
-prob.stol = 1e-6
+prob.stol = 1e-10
 if prob.rolling < 64:
-    prob.stol = 1e-9
+    prob.stol = 1e-13
 
 prob.m0 = 10 * (prob.T_end - prob.T_start)/prob.rolling
 
