11235cf11b8a19c0b9a12c0041a13cc8f4928a3d
Gayatri ?aklovi? <aklovi1@jwlogin08.juwels>
2021-01-22 18:05:32 +0100

    wtf is result heat3_stron no 11?

--- test_runs_and_plots/problem_scaling/heat3_strong/output/000009/000000_sub_step/work/run.py
+++ test_runs_and_plots/problem_scaling/heat3_strong/output/000009/000000_sub_step/work/run.py
@@ -14,7 +14,9 @@ from problem_examples_parallel.heat_2d_pbc_central6 import Heat as Heat6
 prob = Heat6()
 N = 350
 prob.spatial_points = [N, N]
-prob.tol = 1e-12
+prob.tol = max(1e-12 / prob.rolling, 8e-13)
+if prob.rolling == 64:
+    prob.tol = 1e-12
 prob.proc_col = 1
 prob.time_points = 3
 prob.optimal_alphas = True
@@ -22,9 +24,12 @@ prob.T_start = np.pi
 prob.solver = 'custom'
 prob.maxiter = 10
 prob.smaxiter = 100
-prob.stol = 1e-14
+prob.stol = 1e-15
 prob.T_end += np.pi
 prob.m0 = (prob.T_end - prob.T_start)/prob.rolling
+
+if prob.rolling < 64:
+    prob.stol = prob.tol / 10
 
 
 prob.setup()
