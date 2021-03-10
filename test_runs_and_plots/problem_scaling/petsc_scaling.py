41bcc8c4cbf4430351137513bd7d8a4669a8eff2
Gaya <caklovicka@gmail.com>
2021-03-10 13:43:29 +0100

    adv1 petsc scaling

--- test_runs_and_plots/problem_scaling/petsc_scaling.py
+++ test_runs_and_plots/problem_scaling/petsc_scaling.py
@@ -4,16 +4,17 @@ import seaborn as sns
 from matplotlib.lines import Line2D
 
 
-path = ['adv3_strong/output/000002/result/result.dat']
+path = ['adv1_strong/output/000002/result/result.dat']
 
 # nproc | tot_time |
-eq = np.loadtxt(path[0], delimiter='|', usecols=[0, 1], skiprows=3)
+eq = np.loadtxt(path[0], delimiter='|', usecols=[0, 3], skiprows=3)
+n = eq.shape[0]
 seqT = max(eq[:, 1])
 print(eq)
 eq[:, 1] = seqT / eq[:, 1]
 
-
 plt.plot(np.log2(eq[:, 0]), eq[:, 1])
-plt.plot(np.log2(eq[:, 0]), [1, 2, 4, 8, 16, 32, 64])
-plt.xticks(np.log2(eq[:, 0]), eq[:, 0])
+plt.plot(range(n-3), 2**np.array(range(n-3)))
+plt.xticks(range(8), eq[:, 0])
+plt.legend(['petsc-juwels', 'ideal'])
 plt.show()
