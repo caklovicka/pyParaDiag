3c52d300ce88e06c3dd1870262ab5736a456bae2
Gaya <caklovicka@gmail.com>
2021-03-10 16:32:11 +0100

    adv1 strong scaling on 512

--- test_runs_and_plots/problem_scaling/petsc_scaling.py
+++ test_runs_and_plots/problem_scaling/petsc_scaling.py
@@ -4,19 +4,23 @@ import seaborn as sns
 from matplotlib.lines import Line2D
 
 
-path = ['adv1_strong/output/000006/result/result.dat']
+path = ['adv1_strong/output/000006/result/result.dat', 'adv1_strong/output/000007/result/result.dat']
 
 # nproc | tot_time |
-eq = np.loadtxt(path[0], delimiter='|', usecols=[0, 3], skiprows=3)
-n = eq.shape[0]
+eq = []
+for p in path:
+    eq.append(np.loadtxt(p, delimiter='|', usecols=[0, 3], skiprows=3))
+
+n = eq[0].shape[0]
 seqT = 10.3642304
-print(eq)
-eq[:, 1] = seqT / eq[:, 1]
-#
-plt.plot(np.log2(eq[:, 0]), eq[:, 1], 'x')
-# plt.plot(np.log2(eq[:, 0]), eq[:, 0], 'x')
-plt.xticks(np.log2(eq[:, 0]), eq[:, 0])
-plt.legend(['petsc-juwels', 'ideal'])
+for i in range(len(eq)):
+    eq[i][:, 1] = seqT / eq[i][:, 1]
+
+    plt.plot(np.log2(eq[i][:, 0]), eq[i][:, 1], 'X')
+    # plt.plot(np.log2(eq[:, 0]), eq[:, 0], 'x')
+    plt.xticks(np.log2(eq[i][:, 0]), eq[i][:, 0])
+
+plt.legend(['petsc-6', 'petsc-12', 'ideal'])
 # plt.xlim([0, 5])
 # plt.ylim([0, 15])
 plt.show()
