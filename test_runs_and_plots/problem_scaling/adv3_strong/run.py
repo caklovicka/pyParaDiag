37099225aac0331bc8617456fa36e6333822d9fa
Gaya <caklovicka@gmail.com>
2021-03-10 18:22:54 +0100

    adv2 and adv3 strong scaling with petsc 12 cores

--- test_runs_and_plots/problem_scaling/adv3_strong/run.py
+++ test_runs_and_plots/problem_scaling/adv3_strong/run.py
@@ -13,13 +13,11 @@ import numpy as np
 from problem_examples_parallel.advection_2d_pbc_upwind5 import Advection as Adv5
 
 prob = Adv5()
-N = 704
+N = 702
 prob.spatial_points = [N, N]
 prob.tol = 1e-12
 prob.time_points = 3
-prob.time_intervals = 1
-prob.proc_row = 1
-prob.rolling = 64
+prob.proc_col = 36
 prob.optimal_alphas = True
 prob.T_start = 0
 prob.T_end = 1.28e-2
@@ -28,9 +26,9 @@ prob.maxiter = 10
 prob.smaxiter = 50
 
 prob.stol = 1e-14
-# if prob.rolling < 64:
-#     prob.stol = 1e-15
-#     prob.tol = 1e-12 / prob.rolling
+if prob.rolling < 64:
+    prob.stol = 1e-15
+    prob.tol = 1e-12 / prob.rolling
 
 prob.m0 = 10 * (prob.T_end - prob.T_start)/prob.rolling
 
