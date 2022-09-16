4e461b45ea27a3567292f6824b80f4a19909e703
caklovicka <caklovicka@gmail.com>
2022-09-16 13:12:07 +0200

    changed custom petsc solver to fit version 3.17.4

--- problem_examples_parallel/advection_2d_pbc_upwind1.py
+++ problem_examples_parallel/advection_2d_pbc_upwind1.py
@@ -113,7 +113,7 @@ class Advection(LinearParalpha):
         M.createAIJ(size=(self.global_size_A, self.global_size_A), csr=csr, comm=self.comm_matrix)
 
         ksp = PETSc.KSP()
-        ksp.create()
+        ksp.create(comm=self.comm_matrix)
         ksp.setType('gmres')
         ksp.setFromOptions()
         ksp.setTolerances(rtol=tol, max_it=self.smaxiter)
--- problem_examples_parallel/heat_2d_pbc_central6.py
+++ problem_examples_parallel/heat_2d_pbc_central6.py
@@ -172,11 +172,10 @@ class Heat(LinearParalpha):
         M.createAIJ(size=(self.global_size_A, self.global_size_A), csr=csr, comm=self.comm_matrix)
 
         ksp = PETSc.KSP()
-        ksp.create()
+        ksp.create(comm=self.comm_matrix)
         ksp.setType('gmres')
         ksp.setFromOptions()
-        # ksp.setTolerances(rtol=tol, atol=tol, max_it=self.smaxiter)
-        ksp.setTolerances(rtol=self.stol, max_it=self.smaxiter)
+        ksp.setTolerances(rtol=tol, max_it=self.smaxiter)
         pc = ksp.getPC()
         pc.setType('none')
         ksp.setOperators(M)
