dd12f4849541abfb322679f9839b894aef392161
caklovicka <caklovicka@gmail.com>
2022-09-28 16:05:42 +0200

    testing a constant alpha with adv3.py

---
+++ test_runs_and_plots/adv3.py
@@ -0,0 +1,49 @@
+# the following lines disable the numpy multithreading [optional]
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
+import sys
+sys.path.append('../..')
+
+from problem_examples_parallel.advection_2d_pbc_upwind5 import Advection
+prob = Advection()
+
+# choosing a number of points
+prob.spatial_points = [600, 600]
+prob.time_points = 3
+
+# choosing a time domain
+prob.T_start = 0
+prob.T_end = 1e-2
+prob.m0 = 1e-2
+
+# choosing the number of intervals handled in parallel
+prob.time_intervals = 32
+prob.rolling = 1
+
+# choosing a parallelization strategy
+prob.proc_col = 1
+prob.proc_row = prob.time_intervals
+
+# choosing a solver
+prob.solver = 'gmres'
+
+# setting maximum number of iterations
+prob.maxiter = 15
+prob.smaxiter = 100
+
+# choosing a setting for the alpha sequence
+prob.optimal_alphas = False
+prob.alphas = [1e-4]
+
+# setting tolerances
+prob.tol = 1e-12
+prob.stol = 1e-14
+
+prob.setup()                                # must be before solve()
+prob.solve()                                # this is where magic happens
+prob.summary(details=True)
