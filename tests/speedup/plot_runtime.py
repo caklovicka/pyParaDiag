d228eafbadb50dede97867ca451f6f14d0226972
caklovicka <caklovicka@gmail.com>
2022-10-20 16:58:20 +0200

    plot drawing

--- tests/speedup/plot_runtime.py
+++ tests/speedup/plot_runtime.py
@@ -3,15 +3,16 @@ import matplotlib
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-alphas = ["1e-4", "1e-8"]
+alphas = ["old, adaptive", "new, 1e-4", "new, 1e-8"]
 equations = ["heat, tol = 1e-5", "heat, tol = 1e-9", "heat, tol = 1e-12", "advection, tol = 1e-5", "advection, tol = 1e-9", "advection, tol = 1e-12"]
 
-runtimes = np.array([[0.21547, 16.40066, 29.19395, 0.70112, 3.76745, 32.19536],
+runtimes = np.array([[0.26107, 0.92291, 4.66083, 1.29374, 5.47151, 40.11753],
+                    [0.21547, 16.40066, 29.19395, 0.70112, 3.76745, 32.19536],
                     [9.47420, 26.45528, 146.59290, 0.70069, 1.95702, 17.31436]])
 
 
 fig, ax = plt.subplots()
-im = ax.imshow(runtimes)
+im = ax.imshow(runtimes, cmap='Reds')
 
 # Show all ticks and label them with the respective list entries
 ax.set_yticks(np.arange(len(alphas)), labels=alphas)
@@ -23,8 +24,8 @@ plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
 # Loop over data dimensions and create text annotations.
 for i in range(len(alphas)):
     for j in range(len(equations)):
-        text = ax.text(j, i, runtimes[i, j], ha="center", va="center", color="w")
+        text = ax.text(j, i, runtimes[i, j], ha="center", va="center", color="g")
 
-ax.set_title("Runtime, refinement in seconds")
+ax.set_title("Runtime in seconds")
 fig.tight_layout()
 plt.show()
