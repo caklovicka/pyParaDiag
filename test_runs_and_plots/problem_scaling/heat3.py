44898372be2934e3b9db5caf421cb95f1d6eda21
caklovicka <caklovicka@gmail.com>
2022-10-20 13:20:20 +0200

    something was wron with tols

--- test_runs_and_plots/problem_scaling/heat3.py
+++ test_runs_and_plots/problem_scaling/heat3.py
@@ -1,4 +1,3 @@
-# the following lines disable the numpy multithreading [optional]
 import os
 os.environ["OMP_NUM_THREADS"] = "1"
 os.environ["OPENBLAS_NUM_THREADS"] = "1"
@@ -8,42 +7,40 @@ os.environ["NUMEXPR_NUM_THREADS"] = "1"
 
 import sys
 sys.path.append('../..')
+from mpi4py import MPI
 import numpy as np
+from problem_examples_parallel.advection_2d_pbc_upwind5 import Advection as Adv5
 
-from problem_examples_parallel.heat_2d_pbc_central6 import Heat
-prob = Heat()
-
-# choosing a number of points
-prob.spatial_points = [350, 350]
-prob.time_points = 3
-
-# choosing a time domain
-prob.T_start = np.pi
-prob.T_end = prob.T_start + 0.16
-
-# choosing the number of intervals handled in parallel
-prob.time_intervals = 64
-prob.rolling = 1
-
-# choosing a parallelization strategy
-prob.proc_col = 1
-prob.proc_row = prob.time_intervals
-
-# choosing a solver
-prob.solver = 'custom'
-
-# setting maximum number of iterations
-prob.maxiter = 3
-prob.smaxiter = 1000
-
-# choosing a setting for the alpha sequence
-prob.optimal_alphas = True
-prob.m0 = prob.m0 = 10 * (prob.T_end - prob.T_start)/prob.rolling
-
-# setting tolerances
+prob = Adv5()
+N = 700
+prob.spatial_points = [N, N]
 prob.tol = 1e-12
-prob.stol = 1e-14
+prob.time_points = 3
+prob.proc_col = 1
+prob.optimal_alphas = True
+prob.T_start = 0
+prob.T_end = 1.28e-2
+prob.solver = 'custom'
+prob.maxiter = 10
+prob.smaxiter = 50
 
-prob.setup()                                # must be before solve()
-prob.solve()                                # this is where magic happens
+prob.stol = 1e-14
+if prob.rolling < 64:
+    prob.stol = 1e-15
+    prob.tol = 1e-12 / prob.rolling
+
+prob.m0 = 10 * (prob.T_end - prob.T_start)/prob.rolling
+
+prob.setup()
+prob.solve()
 prob.summary(details=True)
+
+if prob.rank == prob.size - 1: #prob.size_subcol_seq:
+    exact = prob.u_exact(prob.T_end, prob.x).flatten()[prob.row_beg:prob.row_end]
+    approx = prob.u_last_loc.flatten()
+    d = exact - approx
+    d = d.flatten()
+    err_abs = np.linalg.norm(d, np.inf)
+    # err_abs_root = prob.comm_subcol_seq.reduce(err_abs, op=MPI.MAX, root=prob.size_subcol_seq - 1)
+    # if prob.rank == prob.size - 1:
+    print('abs err = {}'.format(err_abs))
