cef7d71c7ff5d011ed3cc91528349c16c5a3871c
caklovicka <caklovicka@gmail.com>
2022-11-07 10:57:49 +0100

    bug fix?

--- core/helpers.py
+++ core/helpers.py
@@ -527,9 +527,14 @@ class Helpers(Communicators):
 
     def __bcast_u_last_loc__(self):
 
-        if self.comm_last != MPI.COMM_NULL and self.time_intervals > 1:  # and self.size_col < self.size:
+        if self.betas == NotImplemented:
+            if self.comm_last != MPI.COMM_NULL and self.time_intervals > 1:  # and self.size_col < self.size:
+                time_beg = MPI.Wtime()
+                self.u_last_loc = self.comm_last.bcast(self.u_last_loc, root=0)
+                self.communication_time += MPI.Wtime() - time_beg
+        else:
             time_beg = MPI.Wtime()
-            self.u_last_loc = self.comm_last.bcast(self.u_last_loc, root=0)
+            self.u_last_loc = self.comm_row.bcast(self.u_last_loc, root=self.size_row - 1)
             self.communication_time += MPI.Wtime() - time_beg
 
     def __write_time_in_txt__(self):
