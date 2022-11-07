f128ba830c994982d55fa6e4e538d1031a65c0e5
caklovicka <caklovicka@gmail.com>
2022-11-07 18:11:02 +0100

    testing

--- tests/scaling/eps 1/ac1_1.py
+++ tests/scaling/eps 1/ac1_1.py
@@ -18,29 +18,29 @@ import numpy as np
 
 from examples.nonlinear.allen_cahn_2d_pbc_central2 import AllenCahn
 prob = AllenCahn()
-prob.spatial_points = [800, 800]
+prob.spatial_points = [1000, 1000]
 prob.time_points = 2
 prob.tol = 1e-5
 prob.stol = 1e-7
 
-prob.eps = 0.1
+prob.eps = 1
 prob.T_start = 0
-prob.T_end = 0.125
 prob.proc_col = 1
 prob.solver = 'gmres'
 prob.maxiter = 20
 prob.smaxiter = 500
 prob.alphas = [1e-8]
-prob.R = 0.5
+prob.R = 1
 prob.X_left = -2 * prob.R
 prob.X_right = 2 * prob.R
 prob.Y_left = -2 * prob.R
 prob.Y_right = 2 * prob.R
+prob.T_end = prob.R ** 2 * 0.4
 
 prob.proc_row = prob.time_intervals
 
 prob.setup()
-print(prob.dx[0]**2, prob.dt**(2 * prob.time_points - 1), prob.dt < prob.eps**2)
+print(prob.T_end, prob.dx[0]**2, prob.dt**(2 * prob.time_points - 1), prob.dt < prob.eps**2)
 print(prob.eps, prob.eps**2, prob.eps**3)
 prob.solve()
 prob.summary(details=False)
