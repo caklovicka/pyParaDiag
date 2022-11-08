676c70d526e00320d894d97082efc77b97f7ca74
caklovicka <caklovicka@gmail.com>
2022-11-08 09:54:33 +0100

    for jube

--- tests/scaling/eps 1/ac1_1.py
+++ tests/scaling/eps 1/ac1_1.py
@@ -16,31 +16,32 @@ import numpy as np
 # time_intervals from runtime
 # beta from runtime
 
-from examples.nonlinear.allen_cahn_2d_pbc_central4 import AllenCahn
+from examples.nonlinear.allen_cahn_2d_pbc_central2 import AllenCahn
 prob = AllenCahn()
-prob.spatial_points = [200, 200]
-prob.time_points = 2
+prob.spatial_points = [800, 800]
+prob.time_points = 1
 prob.tol = 1e-5
 prob.stol = 1e-7
 
 prob.eps = 1
 prob.T_start = 0
 prob.proc_col = 1
-prob.solver = 'gmres'
+prob.solver = 'custom'
 prob.maxiter = 20
 prob.smaxiter = 500
 prob.alphas = [1e-8]
-prob.R = 2
+prob.R = 1
 prob.X_left = -2 * prob.R
 prob.X_right = 2 * prob.R
 prob.Y_left = -2 * prob.R
 prob.Y_right = 2 * prob.R
-prob.T_end = 1
+prob.T_end = 0.001
 
 prob.proc_row = prob.time_intervals
 
 prob.setup()
-print(prob.T_end, prob.dx[0]**4, prob.dt**(2 * prob.time_points - 1), prob.dt < prob.eps**2)
+print(prob.T_end < prob.R ** 2 / 2)
+print(prob.T_end, prob.dx[0]**2, prob.dt**(2 * prob.time_points - 1), prob.dt < prob.eps**2)
 print(prob.eps, prob.eps**2, prob.eps**3)
 prob.solve()
 prob.summary(details=False)
