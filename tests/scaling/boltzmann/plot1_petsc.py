c86cac5d2011bf100f964983e5c402ca089429d0
caklovicka <caklovicka@gmail.com>
2022-12-16 11:14:15 +0100

    for petsc 32/n

--- tests/scaling/boltzmann/plot1_petsc.py
+++ tests/scaling/boltzmann/plot1_petsc.py
@@ -5,6 +5,7 @@ import seaborn as sns
 # run0: petsc scaling (1, 2, 4, 8, 16, 32, 64)
 # run2: petsc(32), 64/n
 # run3: petsc(64), 64/n
+# run4: petsc(64), 32/n
 
 legend = []
 petsc_proc = []
@@ -25,8 +26,6 @@ for i in range(table.shape[0]):
 plt.semilogy(petsc_proc, petsc_time, 'X-', color='gray', markersize=10)
 plt.semilogy(petsc_proc, seq_time / (2 ** np.array(petsc_proc)), 'X:', color='gray', markersize=10)
 
-petsc_time_32 = petsc_time[-2]
-
 # PinT + PETSc
 K = 1
 mksz = 15
@@ -36,25 +35,27 @@ pint_petsc_proc = []
 pint_petsc_time = []
 pint_petsc_iters = []
 
-runs = [2]
+runs = [3]
 
 for k in range(K):
-    # nproc | time | tot iters
-    table = np.loadtxt('output1/00000{}/result/result.dat'.format(runs[k]), delimiter='|', skiprows=3, usecols=[1, 4, 7])
+    # nproc | proc_col | time | tot iters
+    table = np.loadtxt('output1/00000{}/result/result.dat'.format(runs[k]), delimiter='|', skiprows=3, usecols=[1, 2, 5, 8])
 
     legend.append('petsc + pint ' + str(k + 1))
     pint_petsc_proc.append([])
     pint_petsc_time.append([])
     pint_petsc_iters.append([])
 
-    pint_petsc_proc[k].append(np.log2(32))
-    pint_petsc_time[k].append(petsc_time_32)
-    pint_petsc_iters[k].append('$32$')
+    proc_col = table[0, 1]
+    pint_petsc_proc[k].append(np.log2(int(proc_col)))
+    petsc_time_seq = petsc_time[int(pint_petsc_proc[k][-1])]
+    pint_petsc_time[k].append(petsc_time_seq)
+    pint_petsc_iters[k].append('$' + str(int(proc_col)) + '$')
 
     for i in range(table.shape[0]):
         pint_petsc_proc[k].append(np.log2(table[i, 0]))
-        pint_petsc_time[k].append(table[i, 1])
-        pint_petsc_iters[k].append('$' + str(int(table[i, 2])) + '$')
+        pint_petsc_time[k].append(table[i, 2])
+        pint_petsc_iters[k].append('$' + str(int(table[i, 3])) + '$')
 
     for i in range(len(pint_petsc_iters[k])):
         plt.semilogy(pint_petsc_proc[k][i], pint_petsc_time[k][i], color=col[k], markersize=mksz, marker=pint_petsc_iters[k][i])
