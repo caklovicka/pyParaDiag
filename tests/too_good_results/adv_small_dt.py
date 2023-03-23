adc4fd01385783541d4cda44c19fdd1f6e071c34
caklovicka <caklovicka@gmail.com>
2023-03-23 12:45:27 +0100

    presenting too good results

--- tests/too_good_results/adv_small_dt.py
+++ tests/too_good_results/adv_small_dt.py
@@ -10,23 +10,27 @@ import sys
 sys.path.append('../../..')                 # for core
 sys.path.append('../../../../../../..')     # for jube
 import numpy as np
-from problem_examples_parallel.heat_2d_pbc_central6 import Heat as Heat6
+from problem_examples_parallel.advection_2d_pbc_upwind5 import Advection as Adv5
 
-prob = Heat6()
-N = 350
+prob = Adv5()
+N = 700
 prob.spatial_points = [N, N]
 prob.tol = 1e-12
-prob.proc_col = 3
 prob.time_points = 3
+prob.proc_col = 3
 prob.optimal_alphas = True
-prob.T_start = np.pi
-prob.T_end = prob.T_start + 0.016 #0.16
+prob.T_start = 0
+prob.T_end = 1.28e-2 / 100
 prob.solver = 'custom'
 prob.maxiter = 10
 prob.smaxiter = 50
-prob.stol = 1e-13
-prob.m0 = (prob.T_end - prob.T_start)/prob.rolling
 
+prob.stol = 1e-14
+if prob.rolling < 64:
+    prob.stol = 1e-15
+    prob.tol = 1e-12 / prob.rolling
+
+prob.m0 = 10 * (prob.T_end - prob.T_start)/prob.rolling
 
 prob.setup()
 prob.solve()
