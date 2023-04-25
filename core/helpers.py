d906d7bc48e5831f0558345ee8fa8c235ea461ab
caklovicka <caklovicka@gmail.com>
2023-04-25 19:32:02 +0200

    testing something

--- core/helpers.py
+++ core/helpers.py
@@ -470,7 +470,7 @@ class Helpers(Communicators):
             for i in range(self.Frac):
                 d = D[i + self.rank_col * self.Frac]
                 r = d / (1 - d)
-                d = (1 - r) * d
+                d = 1 / (1 - r) * d
                 sys = sc.sparse.eye(self.global_size_A) - self.dt * d * self.Apar
                 if self.solver == 'custom':
                     h1_loc[i * self.global_size_A:(i + 1) * self.global_size_A], it = self.linear_solver(sys, h_loc[i * self.global_size_A:(i + 1) * self.global_size_A], x0[i * self.global_size_A:(i + 1) * self.global_size_A], tol)
